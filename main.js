cfcf6b62dbe2a80142de2cc3e7fc32732ebc8c0b
Saurabh Totey <SaurabhTotey@gmail.com>
2018-04-21 18:56:54 -0600

    Made image reduction function and updated documentation

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*
  * JavaScript code to make a large image out of a bunch of other smaller images
  * Smaller images can be clicked to become the large image made up of smaller images
- * All images are selected at random from Google Images
+ * All images are selected at random from Flickr
  */
 
-//Gets the mosiac table that will store an image in each cell to become the larger image
+//Gets the mosiac element that will store a bunch of images to become a larger image
 var mosaic = document.getElementById('mosaic-container');
 mosaic.width = window.innerWidth;
 mosaic.height = window.innerHeight;
@@ -13,12 +13,12 @@ mosaic.height = window.innerHeight;
 var outerResolution = 75;
 var innerResolution = Math.min(window.innerHeight, window.innerWidth) / outerResolution;
 
-//How each cell location translates to an ID name
+//How each image location translates to an ID name
 function idForLocation(row, column) {
     return "r:" + row + ";c:" + column;
 }
 
-//Initializes the table cells
+//Initializes the image elements
 for (var i = 0; i < outerResolution; i++) {
     for (var j = 0; j < outerResolution; j++) {
         mosaic.appendChild(document.createElement('img'));
@@ -29,7 +29,7 @@ for (var i = 0; i < outerResolution; i++) {
     mosaic.appendChild(document.createElement('br'));
 }
 
-//Takes in the image and sets its source to a random image from Flickr
+//Takes in an image and sets its source to a random image from Flickr
 function setSourceToRandom(image) {
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
         {
@@ -43,15 +43,9 @@ function setSourceToRandom(image) {
     );
 }
 
-//TODO: temporary
-for (var i = 0; i < outerResolution; i++) {
-    for (var j = 0; j < outerResolution; j++) {
-        setSourceToRandom(document.getElementById(idForLocation(i, j)));
-    }
-}
-
 //The actual function that handles making a mosaic of an image
 function makeMosaicOf(image) {
+    //Gets a 2d array of colors that represents the image; dimensions are outerResolution x outerResolution
     function getPixelDataForImage(image) {
         var pixelData = document.createElement('canvas');
         pixelData.width = outerResolution;
@@ -65,5 +59,24 @@ function makeMosaicOf(image) {
             }
         }
         return data;
+    }
+    //Gets the dominant color of an image if stretched to innerResolution x innerResolution
+    function reduceImageToColor(image) {
+        var pixelData = document.createElement('canvas');
+        pixelData.width = innerResolution;
+        pixelData.height = innerResolution;
+        pixelData.getContext('2d').drawImage(image, 0, 0);
+        colorCounts = {};
+        for (var i = 0; i < innerResolution; i++) {
+            for (var j = 0; j < innerResolution; j++) {
+                var color = pixelData.getImageData(j, i, 1, 1).data;
+                if (!color in colorCounts) {
+                    colorCounts[color] = 1;
+                } else {
+                    colorCounts[color] += 1;
+                }
+            }
+        }
+        return Object.keys(colorCounts).reduce((a, b) => obj[a] > obj[b] ? a : b);
     }
 }
